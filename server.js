dddda56cdf66cce4f54225f4df6656f7f0577eaf
yusheng <poallin@gmail.com>
2016-08-28 02:46:59 +0800

    modified

--- server.js
+++ server.js
@@ -1,28 +1,17 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser')
+const db = require('./db');
+const blogRouter = require('./routers/blogs');
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3030;
 
 app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname,'public')));
-
-const router = express.Router();
-
-router.use(function(req,res,next){
-  console.log(req.method,req.url);
-    // continue doing what we were doing and go to the route
-  next();
-});
-
-router.get('/',function(req,res){
-  res.render('index');
-});
-
-
-// apply the routes to our application
-app.use('/', router);
-
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({ extended: false }));
+app.use('/blogs',blogRouter);
 app.listen(port,function(){
   console.log('app is running on:' + port);
 });
